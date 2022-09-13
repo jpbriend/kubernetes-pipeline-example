de9502275b2e301e096dcaafad3b584b621981a2
Jean-Philippe Briend <jeanphilippe.briend@gmail.com>
2022-09-13 11:21:47 +0200

    Upgrade to latest version of Spring Boot

--- src/test/java/com/cloudbees/example/mobile/deposit/api/DepositEndpointTests.java
+++ src/test/java/com/cloudbees/example/mobile/deposit/api/DepositEndpointTests.java
@@ -1,19 +1,19 @@
 package com.cloudbees.example.mobile.deposit.api;
 
 import com.cloudbees.example.mobile.deposit.api.model.Deposit;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = MobileDepositApplication.class, webEnvironment=RANDOM_PORT)
 public class DepositEndpointTests {
 
